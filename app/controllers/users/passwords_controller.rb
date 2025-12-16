b1cab27788f35295320d776fff583301aa7e2607
vero <veronica.garcia@alten.es>
2025-12-16 12:29:12 +0100

    Modificaciones para que vaya  a la pnatalla correcta al resetear contraseña

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -3,7 +3,7 @@ class Users::PasswordsController < Devise::PasswordsController
     Rails.logger.info "=" * 80
     Rails.logger.info "PASSWORDS#update PERSONALIZADO RESET"
     
-    # Sobrescribir reset_password_by_token para skipear validaci�n de gender
+    # Sobrescribir reset_password_by_token para skipear validaci�n de gender
     original_token = resource_params[:reset_password_token]
     reset_password_token = Devise.token_generator.digest(User, :reset_password_token, original_token)
     self.resource = User.find_or_initialize_with_error_by(:reset_password_token, reset_password_token)
@@ -12,11 +12,11 @@ class Users::PasswordsController < Devise::PasswordsController
     
     if resource.persisted?
       if resource.reset_password_period_valid?
-        # IMPORTANTE: Skipear validaci�n ANTES del reset_password
+        # IMPORTANTE: Skipear validaci�n ANTES del reset_password
         resource.skip_gender_validation = true
         Rails.logger.info "skip_gender_validation asignado: #{resource.skip_gender_validation}"
         
-        # Ahora s� hacer el reset
+        # Ahora s� hacer el reset
         if resource_params[:password].present?
           resource.password = resource_params[:password]
           resource.password_confirmation = resource_params[:password_confirmation]
@@ -37,6 +37,7 @@ class Users::PasswordsController < Devise::PasswordsController
     if resource.errors.empty?
       resource.unlock_access! if unlockable?(resource)
       if Devise.sign_in_after_reset_password
+        Rails.logger.error "Devise.sign_in_after_reset_password"
         flash_message = resource.active_for_authentication? ? :updated : :updated_not_active
         set_flash_message!(:notice, flash_message)
         resource.after_database_authentication
@@ -49,6 +50,33 @@ class Users::PasswordsController < Devise::PasswordsController
       Rails.logger.error "FALLO AL GUARDAR PASSWORD"
       set_minimum_password_length
       respond_with resource
+    end
+  end
+  def after_sign_in_path_for(resource)
+    Rails.logger.info "="*50
+    Rails.logger.info "PASSWORDS - ENTRANDO EN after_sign_in_path_for"
+    Rails.logger.info "Resource: #{resource.inspect}"
+    Rails.logger.info "Sexo: #{resource.gender.inspect}"
+    Rails.logger.info "="*50
+    
+    if resource.gender.nil? && !resource.organization?
+      Rails.logger.info "PASSWORDS - Redirigiendo a account_path por falta de género"
+      account_path
+    elsif resource.show_welcome_screen?
+      welcome_path
+    elsif resource.organization? && resource.organization&.rejected_at.present?
+      flash.discard
+      sign_out(resource)
+      flash[:alert] = "Su petición de registro como organización ha sido rechazada."
+      new_user_session_path
+    elsif resource.organization? && !resource.organization&.verified_at.present?
+      flash.discard
+      sign_out(resource)
+      flash[:alert] = "Su petición de registro como organización está pendiente de validación."
+      new_user_session_path
+    else
+      Rails.logger.info "PASSWORDS - Redirigiendo a super (ruta por defecto)"
+      super
     end
   end
 end
