aaad922d6ca78bb887b8aa7aa1db190973b528b0
vero <veronica.garcia@alten.es>
2026-02-11 13:24:59 +0100

    Modificaciones para que no tenga en cuenta mensajes de la fecha

--- app/models/custom/verification/residence.rb
+++ app/models/custom/verification/residence.rb
@@ -20,9 +20,7 @@ class Verification::Residence
   end
 
   def save
-    Rails.logger.info "--- Se comprueba la fecha de nacimiento censo: #{date_of_birth}   ---" 
-    Rails.logger.info "--- Se comprueba la fecha de nacimiento censo: #{postal_code}   ---" 
-    
+       
     Rails.logger.info "--- se comprueba si el padrón devuelve un usuario---"
     if !valid? || !@census_data.valid?
       Rails.logger.info "--- El padrón no devuelve respuesta válida --"
@@ -38,28 +36,7 @@ class Verification::Residence
       return false
     end
 
-    Rails.logger.info "--- Se comprueba el codigo postal  ---"   
-      
-    if @census_data.postal_code != postal_code
-      Rails.logger.info "--- El padrón devuelve un CPostal disitinto al del usuario --"
-      Rails.logger.info "--- Se comprueba la fecha de nacimiento censo: #{@census_data.postal_code}   ---" 
-      Rails.logger.info "--- Se comprueba la fecha de nacimiento censo: #{postal_code}   ---" 
-      errors.add(:error_cp, I18n.t("verification.residence.new.error_verifying_data"))
-      return false
-    end
-
     
-    Rails.logger.info "--- Se comprueba la fecha de nacimiento  ---"    
-    if @census_data.date_of_birth != date_of_birth
-      Rails.logger.info "--- El padrón devuelve una fecha de naciemiento disitinta al del usuario --"
-      Rails.logger.info "--- Se comprueba la fecha de nacimiento censo: #{@census_data.date_of_birth}   ---" 
-      Rails.logger.info "--- Se comprueba la fecha de nacimiento censo: #{date_of_birth}   ---" 
-      errors.add(:error_date, I18n.t("verification.residence.new.error_verifying_data"))
-      return false
-    end
-
-    
-
     user.take_votes_if_erased_document(document_number, document_type)
 
     user.update(document_number:       document_number,
