37ad483009da9d754c4964d2aebccd712d033907
vero <veronica.garcia@alten.es>
2026-04-29 10:04:16 +0200

    Modificado para borrar bien los evaluadores , borrando previamente su asociación a los presupuestos

--- app/models/valuator.rb
+++ app/models/valuator.rb
@@ -20,4 +20,28 @@ class Valuator < ApplicationRecord
   def assigned_investment_ids
     investment_ids + valuator_group&.investment_ids.to_a
   end
+
+  # Elimina los budget_valuators antes de eliminar el evaluador
+  before_destroy :destroy_budget_valuators
+
+  private
+
+  def destroy_budget_valuators
+    Rails.logger.info "=== ELIMINANDO LÓGICAMENTE BUDGET VALUATORS ==="
+    Rails.logger.info "User ID: #{self.user_id}"
+    
+     valuator = Valuator.find_by(user_id: self.user_id)
+
+    if valuator
+      budget_valuators = BudgetValuator.where(valuator: valuator)
+      Rails.logger.info "BudgetValuators encontrados: #{budget_valuators.count}"
+      
+      count = budget_valuators.count
+      budget_valuators.destroy_all
+      Rails.logger.info "BudgetValuators eliminados: #{count}"
+    else
+      Rails.logger.info "No se encontró Valuator para user_id: #{self.user_id}"
+    end
+  end
+
 end
